94d61d6e359588d3c04a308b22ae51c3c82d05a1
Guilherme Ribeiro <guilhemerps94@gmail.com>
2020-09-27 00:45:40 -0300

    Novamente apenas um teste com nome de usuário

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -5,7 +5,7 @@ using dotnetWebApiRest.Data;
 using System.Linq;
 using dotnetWebApiRest.HATEOAS;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Authorization; //teste para veririfcar se o push foi com meu nome  mais um teste */
+using Microsoft.AspNetCore.Authorization;
 
 namespace dotnetWebApiRest.Controllers
 {
